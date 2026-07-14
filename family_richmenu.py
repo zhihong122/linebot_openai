b6bff8dafde05b6dab12a49af746d9e23e1d3a10
zhihong122 <56855836+zhihong122@users.noreply.github.com>
2026-07-14 23:37:13 +0800

    Update family_richmenu.py

--- family_richmenu.py
+++ family_richmenu.py
@@ -1,293 +1,114 @@
+import os
+
 from richmenu_common import create_rich_menu_set
 
 
-BASE_URL = (
-    "https://raw.githubusercontent.com/"
-    "zhihong122/linebot_openai/master/static/family"
-)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+IMAGE_DIR = os.path.join(BASE_DIR, "static", *"family".split("/"))
 
 
-MENU_DEFINITIONS = {
-    "main": {
-        "menu": {
-            "size": {
-                "width": 2500,
-                "height": 1686,
-            },
-            "selected": True,
-            "name": "家屬主選單",
-            "chatBarText": "查看更多資訊",
-            "areas": [
-                {
-                    "bounds": {
-                        "x": 0,
-                        "y": 0,
-                        "width": 833,
-                        "height": 843,
-                    },
-                    "action": {
-                        "type": "richmenuswitch",
-                        "richMenuAliasId": "family_monitoring",
-                        "data": "switch-to-family-monitoring",
-                    },
-                },
-                {
-                    "bounds": {
-                        "x": 834,
-                        "y": 0,
-                        "width": 833,
-                        "height": 843,
-                    },
-                    "action": {
-                        "type": "richmenuswitch",
-                        "richMenuAliasId": "family_management",
-                        "data": "switch-to-family-management",
-                    },
-                },
-                {
-                    "bounds": {
-                        "x": 1663,
-                        "y": 0,
-                        "width": 837,
-                        "height": 843,
-                    },
-                    "action": {
-                        "type": "richmenuswitch",
-                        "richMenuAliasId": "family_medication",
-                        "data": "switch-to-family-medication",
-                    },
-                },
-                {
-                    "bounds": {
-                        "x": 0,
-                        "y": 843,
-                        "width": 833,
-                        "height": 843,
-                    },
-                    "action": {
-                        "type": "richmenuswitch",
-                        "richMenuAliasId": "family_calendar",
-                        "data": "switch-to-family-calendar",
-                    },
-                },
-                {
-                    "bounds": {
-                        "x": 834,
-                        "y": 843,
-                        "width": 833,
-                        "height": 843,
-                    },
-                    "action": {
-                        "type": "richmenuswitch",
-                        "richMenuAliasId": "family_report",
-                        "data": "switch-to-family-report",
-                    },
-                },
-                {
-                    "bounds": {
-                        "x": 1663,
-                        "y": 843,
-                        "width": 837,
-                        "height": 843,
-                    },
-                    "action": {
-                        "type": "richmenuswitch",
-                        "richMenuAliasId": "family_settings",
-                        "data": "switch-to-family-settings",
-                    },
-                },
-            ],
-        },
-        "image": "family_main_menu.jpg",
-        "alias": "family_main",
-    },
-
-    "monitoring": {
-        "menu": {
-            "size": {
-                "width": 2500,
-                "height": 1686,
-            },
-            "selected": True,
-            "name": "家屬監控中心",
-            "chatBarText": "返回主選單",
-            "areas": [
-                {
-                    "bounds": {
-                        "x": 1663,
-                        "y": 843,
-                        "width": 837,
-                        "height": 843,
-                    },
-                    "action": {
-                        "type": "richmenuswitch",
-                        "richMenuAliasId": "family_main",
-                        "data": "switch-to-family-main",
-                    },
-                },
-            ],
-        },
-        "image": "family_monitoring_menu.jpg",
-        "alias": "family_monitoring",
-    },
-
-    "management": {
-        "menu": {
-            "size": {
-                "width": 2500,
-                "height": 1686,
-            },
-            "selected": True,
-            "name": "家屬家庭管理",
-            "chatBarText": "返回主選單",
-            "areas": [
-                {
-                    "bounds": {
-                        "x": 1663,
-                        "y": 843,
-                        "width": 837,
-                        "height": 843,
-                    },
-                    "action": {
-                        "type": "richmenuswitch",
-                        "richMenuAliasId": "family_main",
-                        "data": "switch-to-family-main",
-                    },
-                },
-            ],
-        },
-        "image": "family_management_menu.jpg",
-        "alias": "family_management",
-    },
-
-    "medication": {
-        "menu": {
-            "size": {
-                "width": 2500,
-                "height": 1686,
-            },
-            "selected": True,
-            "name": "家屬藥物管理",
-            "chatBarText": "返回主選單",
-            "areas": [
-                {
-                    "bounds": {
-                        "x": 1663,
-                        "y": 843,
-                        "width": 837,
-                        "height": 843,
-                    },
-                    "action": {
-                        "type": "richmenuswitch",
-                        "richMenuAliasId": "family_main",
-                        "data": "switch-to-family-main",
-                    },
-                },
-            ],
-        },
-        "image": "family_medication_menu.jpg",
-        "alias": "family_medication",
-    },
-
-    "calendar": {
-        "menu": {
-            "size": {
-                "width": 2500,
-                "height": 1686,
-            },
-            "selected": True,
-            "name": "家屬行事曆",
-            "chatBarText": "返回主選單",
-            "areas": [
-                {
-                    "bounds": {
-                        "x": 1663,
-                        "y": 843,
-                        "width": 837,
-                        "height": 843,
-                    },
-                    "action": {
-                        "type": "richmenuswitch",
-                        "richMenuAliasId": "family_main",
-                        "data": "switch-to-family-main",
-                    },
-                },
-            ],
-        },
-        "image": "family_calendar_menu.jpg",
-        "alias": "family_calendar",
-    },
-
-    "report": {
-        "menu": {
-            "size": {
-                "width": 2500,
-                "height": 1686,
-            },
-            "selected": True,
-            "name": "家屬報告紀錄",
-            "chatBarText": "返回主選單",
-            "areas": [
-                {
-                    "bounds": {
-                        "x": 1663,
-                        "y": 843,
-                        "width": 837,
-                        "height": 843,
-                    },
-                    "action": {
-                        "type": "richmenuswitch",
-                        "richMenuAliasId": "family_main",
-                        "data": "switch-to-family-main",
-                    },
-                },
-            ],
-        },
-        "image": "family_report_menu.jpg",
-        "alias": "family_report",
-    },
-
-    "settings": {
-        "menu": {
-            "size": {
-                "width": 2500,
-                "height": 1686,
-            },
-            "selected": True,
-            "name": "家屬系統設定",
-            "chatBarText": "返回主選單",
-            "areas": [
-                {
-                    "bounds": {
-                        "x": 1663,
-                        "y": 843,
-                        "width": 837,
-                        "height": 843,
-                    },
-                    "action": {
-                        "type": "richmenuswitch",
-                        "richMenuAliasId": "family_main",
-                        "data": "switch-to-family-main",
-                    },
-                },
-            ],
-        },
-        "image": "family_settings_menu.jpg",
-        "alias": "family_settings",
-    },
-}
+MENU_DEFINITIONS = {'main': {'menu': {'size': {'width': 2500, 'height': 1686},
+                   'selected': True,
+                   'name': '家屬主選單',
+                   'chatBarText': '查看更多資訊',
+                   'areas': [{'bounds': {'x': 0, 'y': 0, 'width': 833, 'height': 843},
+                              'action': {'type': 'richmenuswitch',
+                                         'richMenuAliasId': 'family_monitoring',
+                                         'data': 'switch-to-family-monitoring'}},
+                             {'bounds': {'x': 834, 'y': 0, 'width': 833, 'height': 843},
+                              'action': {'type': 'richmenuswitch',
+                                         'richMenuAliasId': 'family_management',
+                                         'data': 'switch-to-family-management'}},
+                             {'bounds': {'x': 1663, 'y': 0, 'width': 837, 'height': 843},
+                              'action': {'type': 'richmenuswitch',
+                                         'richMenuAliasId': 'family_medication',
+                                         'data': 'switch-to-family-medication'}},
+                             {'bounds': {'x': 0, 'y': 843, 'width': 833, 'height': 843},
+                              'action': {'type': 'richmenuswitch',
+                                         'richMenuAliasId': 'family_calendar',
+                                         'data': 'switch-to-family-calendar'}},
+                             {'bounds': {'x': 834, 'y': 843, 'width': 833, 'height': 843},
+                              'action': {'type': 'richmenuswitch',
+                                         'richMenuAliasId': 'family_report',
+                                         'data': 'switch-to-family-report'}},
+                             {'bounds': {'x': 1663, 'y': 843, 'width': 837, 'height': 843},
+                              'action': {'type': 'richmenuswitch',
+                                         'richMenuAliasId': 'family_settings',
+                                         'data': 'switch-to-family-settings'}}]},
+          'image': 'family_main_menu.jpg',
+          'alias': 'family_main'},
+ 'monitoring': {'menu': {'size': {'width': 2500, 'height': 1686},
+                         'selected': True,
+                         'name': '家屬監控中心',
+                         'chatBarText': '返回主選單',
+                         'areas': [{'bounds': {'x': 1663, 'y': 843, 'width': 837, 'height': 843},
+                                    'action': {'type': 'richmenuswitch',
+                                               'richMenuAliasId': 'family_main',
+                                               'data': 'switch-to-family-main'}}]},
+                'image': 'family_monitoring_menu.jpg',
+                'alias': 'family_monitoring'},
+ 'management': {'menu': {'size': {'width': 2500, 'height': 1686},
+                         'selected': True,
+                         'name': '家屬家庭管理',
+                         'chatBarText': '返回主選單',
+                         'areas': [{'bounds': {'x': 1663, 'y': 843, 'width': 837, 'height': 843},
+                                    'action': {'type': 'richmenuswitch',
+                                               'richMenuAliasId': 'family_main',
+                                               'data': 'switch-to-family-main'}}]},
+                'image': 'family_management_menu.jpg',
+                'alias': 'family_management'},
+ 'medication': {'menu': {'size': {'width': 2500, 'height': 1686},
+                         'selected': True,
+                         'name': '家屬藥物管理',
+                         'chatBarText': '返回主選單',
+                         'areas': [{'bounds': {'x': 1663, 'y': 843, 'width': 837, 'height': 843},
+                                    'action': {'type': 'richmenuswitch',
+                                               'richMenuAliasId': 'family_main',
+                                               'data': 'switch-to-family-main'}}]},
+                'image': 'family_medication_menu.jpg',
+                'alias': 'family_medication'},
+ 'calendar': {'menu': {'size': {'width': 2500, 'height': 1686},
+                       'selected': True,
+                       'name': '家屬行事曆',
+                       'chatBarText': '返回主選單',
+                       'areas': [{'bounds': {'x': 1663, 'y': 843, 'width': 837, 'height': 843},
+                                  'action': {'type': 'richmenuswitch',
+                                             'richMenuAliasId': 'family_main',
+                                             'data': 'switch-to-family-main'}}]},
+              'image': 'family_calendar_menu.jpg',
+              'alias': 'family_calendar'},
+ 'report': {'menu': {'size': {'width': 2500, 'height': 1686},
+                     'selected': True,
+                     'name': '家屬報告紀錄',
+                     'chatBarText': '返回主選單',
+                     'areas': [{'bounds': {'x': 1663, 'y': 843, 'width': 837, 'height': 843},
+                                'action': {'type': 'richmenuswitch',
+                                           'richMenuAliasId': 'family_main',
+                                           'data': 'switch-to-family-main'}}]},
+            'image': 'family_report_menu.jpg',
+            'alias': 'family_report'},
+ 'settings': {'menu': {'size': {'width': 2500, 'height': 1686},
+                       'selected': True,
+                       'name': '家屬系統設定',
+                       'chatBarText': '返回主選單',
+                       'areas': [{'bounds': {'x': 1663, 'y': 843, 'width': 837, 'height': 843},
+                                  'action': {'type': 'richmenuswitch',
+                                             'richMenuAliasId': 'family_main',
+                                             'data': 'switch-to-family-main'}}]},
+              'image': 'family_settings_menu.jpg',
+              'alias': 'family_settings'}}
 
 
 def create_family_richmenus():
     menu_ids = create_rich_menu_set(
-        role_name="family",
-        base_url=BASE_URL,
+        role_name='family',
+        image_dir=IMAGE_DIR,
         menu_definitions=MENU_DEFINITIONS,
     )
 
     return {
-        "role": "family",
-        "home_rich_menu_id": menu_ids["main"],
+        "role": 'family',
+        "home_rich_menu_id": menu_ids['main'],
         "menus": menu_ids,
         "aliases": {
             key: value["alias"]
